9c49b58ac26f9af601c7b1908292d56e5200af88
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-12-27 10:43:36 +0530

    Update vcmanager.py

--- vcmanager.py
+++ vcmanager.py
@@ -1,29 +1,29 @@
 from telethon import functions
 from telethon.errors import ChatAdminRequiredError, UserAlreadyInvitedError
 from telethon.tl.types import Channel, Chat, User
-from userbot import catub
-from userbot.core.managers import edit_delete, edit_or_reply
-from userbot.helpers.utils import mentionuser
+from Legendbot import legend
+from Legendbot.bot.core.managers import eod, eor
+from Legendbot.helpers.utils import mentionuser
 
-plugin_category = "extra"
+menu_category = "extra"
 
 
 async def get_group_call(chat):
     if isinstance(chat, Channel):
-        result = await catub(functions.channels.GetFullChannelRequest(channel=chat))
+        result = await legend(functions.channels.GetFullChannelRequest(channel=chat))
     elif isinstance(chat, Chat):
-        result = await catub(functions.messages.GetFullChatRequest(chat_id=chat.id))
+        result = await legend(functions.messages.GetFullChatRequest(chat_id=chat.id))
     return result.full_chat.call
 
 
 async def chat_vc_checker(event, chat, edits=True):
     if isinstance(chat, User):
-        await edit_delete(event, "Voice Chats are not available in Private Chats")
+        await eod(event, "Voice Chats are not available in Private Chats")
         return None
     result = await get_group_call(chat)
     if not result:
         if edits:
-            await edit_delete(event, "No Group Call in this chat")
+            await eod(event, "No Group Call in this chat")
         return None
     return result
 
@@ -31,12 +31,12 @@ async def chat_vc_checker(event, chat, edits=True):
 async def parse_entity(entity):
     if entity.isnumeric():
         entity = int(entity)
-    return await catub.get_entity(entity)
+    return await legend.get_entity(entity)
 
 
-@catub.cat_cmd(
+@legend.legend_cmd(
     pattern="vcstart",
-    command=("vcstart", plugin_category),
+    command=("vcstart", menu_category),
     info={
         "header": "To end a stream on Voice Chat.",
         "description": "To end a stream on Voice Chat",
@@ -46,25 +46,25 @@ async def parse_entity(entity):
 )
 async def start_vc(event):
     "To start a Voice Chat."
-    vc_chat = await catub.get_entity(event.chat_id)
+    vc_chat = await legend.get_entity(event.chat_id)
     gc_call = await chat_vc_checker(event, vc_chat, False)
     if gc_call:
-        return await edit_delete(event, "Group Call is already available in this chat")
+        return await eod(event, "Group Call is already available in this chat")
     try:
-        await catub(
+        await legend(
             functions.phone.CreateGroupCallRequest(
                 peer=vc_chat,
-                title="Cat VC",
+                title="LegendBot VC",
             )
         )
-        await edit_delete(event, "Started Group Call")
+        await eod(event, "Started Group Call")
     except ChatAdminRequiredError:
-        await edit_delete(event, "You should be chat admin to start vc", time=20)
+        await eod(event, "You should be chat admin to start vc", time=20)
 
 
-@catub.cat_cmd(
+@legend.legend_cmd(
     pattern="vcend",
-    command=("vcend", plugin_category),
+    command=("vcend", menu_category),
     info={
         "header": "To end a stream on Voice Chat.",
         "description": "To end a stream on Voice Chat",
@@ -74,25 +74,25 @@ async def start_vc(event):
 )
 async def end_vc(event):
     "To end a Voice Chat."
-    vc_chat = await catub.get_entity(event.chat_id)
+    vc_chat = await legend.get_entity(event.chat_id)
     gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
         return
     try:
-        await catub(functions.phone.DiscardGroupCallRequest(call=gc_call))
-        await edit_delete(event, "Group Call Ended")
+        await legend(functions.phone.DiscardGroupCallRequest(call=gc_call))
+        await eod(event, "Group Call Ended")
     except ChatAdminRequiredError:
-        await edit_delete(event, "You should be chat admin to kill vc", time=20)
+        await eod(event, "You should be chat admin to kill vc", time=20)
 
 
-@catub.cat_cmd(
+@legend.legend_cmd(
     pattern="vcinv ?(.*)?",
-    command=("vcinv", plugin_category),
+    command=("vcinv", menu_category),
     info={
         "header": "To invite users on Voice Chat.",
         "usage": "{tr}vcinv < userid/username or reply to user >",
         "examples": [
-            "{tr}vcinv @angelpro",
+            "{tr}vcinv @djkdo",
             "{tr}vcinv userid1 userid2",
         ],
     },
@@ -101,15 +101,15 @@ async def inv_vc(event):
     "To invite users to vc."
     users = event.pattern_match.group(1)
     reply = await event.get_reply_message()
-    vc_chat = await catub.get_entity(event.chat_id)
+    vc_chat = await legend.get_entity(event.chat_id)
     gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
         return
     if not users:
         if not reply:
-            return await edit_delete("Whom Should i invite")
+            return await eod("Whom Should i invite")
         users = reply.from_id
-    await edit_or_reply(event, "Inviting User to Group Call")
+    await eor(event, "Inviting User to Group Call")
     entities = str(users).split(" ")
     user_list = []
     for entity in entities:
@@ -117,17 +117,17 @@ async def inv_vc(event):
         if isinstance(cc, User):
             user_list.append(cc)
     try:
-        await catub(
+        await legend(
             functions.phone.InviteToGroupCallRequest(call=gc_call, users=user_list)
         )
-        await edit_delete(event, "Invited users to Group Call")
+        await eod(event, "Invited users to Group Call")
     except UserAlreadyInvitedError:
-        return await edit_delete(event, "User is Already Invited", time=20)
+        return await eod(event, "User is Already Invited", time=20)
 
 
-@catub.cat_cmd(
+@legend.legend_cmd(
     pattern="vcinfo",
-    command=("vcinfo", plugin_category),
+    command=("vcinfo", menu_category),
     info={
         "header": "To get info of Voice Chat.",
         "usage": "{tr}vcinfo",
@@ -136,12 +136,12 @@ async def inv_vc(event):
 )
 async def info_vc(event):
     "Get info of VC."
-    vc_chat = await catub.get_entity(event.chat_id)
+    vc_chat = await legend.get_entity(event.chat_id)
     gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
         return
-    await edit_or_reply(event, "Getting Group Call Info")
-    call_details = await catub(
+    await eor(event, "Getting Group Call Info")
+    call_details = await legend(
         functions.phone.GetGroupCallRequest(call=gc_call, limit=1)
     )
     grp_call = "**Group Call Info**\n\n"
@@ -156,9 +156,9 @@ async def info_vc(event):
     await edit_or_reply(event, grp_call)
 
 
-@catub.cat_cmd(
+@legend.legend_cmd(
     pattern="vctitle?(.*)?",
-    command=("vctitle", plugin_category),
+    command=("vctitle", menu_category),
     info={
         "header": "To end a stream on Voice Chat.",
         "description": "To end a stream on Voice Chat",
@@ -169,19 +169,19 @@ async def info_vc(event):
 async def title_vc(event):
     "To change vc title."
     title = event.pattern_match.group(1)
-    vc_chat = await catub.get_entity(event.chat_id)
+    vc_chat = await legend.get_entity(event.chat_id)
     gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
         return
     if not title:
-        return await edit_delete("What should i keep as title")
-    await catub(functions.phone.EditGroupCallTitleRequest(call=gc_call, title=title))
-    await edit_delete(event, f"VC title was changed to **{title}**")
+        return await eod("What should i keep as title")
+    await legend(functions.phone.EditGroupCallTitleRequest(call=gc_call, title=title))
+    await eod(event, f"VC title was changed to **{title}**")
 
 
-@catub.cat_cmd(
+@legend.legend_cmd(
     pattern="vc(|un)mute ([\s\S]*)",
-    command=("vcmute", plugin_category),
+    command=("vcmute", menu_category),
     info={
         "header": "To mute users on Voice Chat.",
         "description": "To mute a stream on Voice Chat",
@@ -189,7 +189,7 @@ async def title_vc(event):
             "{tr}vcmute < userid/username or reply to user >",
         ],
         "examples": [
-            "{tr}vcmute @angelpro",
+            "{tr}vcmute @djjkd",
             "{tr}vcmute userid1 userid2",
         ],
     },
@@ -199,16 +199,16 @@ async def mute_vc(event):
     cmd = event.pattern_match.group(1)
     users = event.pattern_match.group(2)
     reply = await event.get_reply_message()
-    vc_chat = await catub.get_entity(event.chat_id)
+    vc_chat = await legend.get_entity(event.chat_id)
     gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
         return
     check = "Unmute" if cmd else "Mute"
     if not users:
         if not reply:
-            return await edit_delete(f"Whom Should i {check}")
+            return await eod(f"Whom Should i {check}")
         users = reply.from_id
-    await edit_or_reply(event, f"{check[:-1]}ing User in Group Call")
+    await eor(event, f"{check[:-1]}ing User in Group Call")
     entities = str(users).split(" ")
     user_list = []
     for entity in entities:
@@ -217,18 +217,18 @@ async def mute_vc(event):
             user_list.append(cc)
 
     for user in user_list:
-        await catub(
+        await legend(
             functions.phone.EditGroupCallParticipantRequest(
                 call=gc_call,
                 participant=user,
                 muted=bool(not cmd),
             )
         )
-    await edit_delete(event, f"{check}d users in Group Call")
+    await eod(event, f"{check}d users in Group Call")
 
 
-@catub.cat_cmd(
-    command=("vcunmute", plugin_category),
+@legend.legend_cmd(
+    command=("vcunmute", menu_category),
     info={
         "header": "To unmute users on Voice Chat.",
         "description": "To unmute a stream on Voice Chat",
@@ -236,7 +236,7 @@ async def mute_vc(event):
             "{tr}vcunmute < userid/username or reply to user>",
         ],
         "examples": [
-            "{tr}vcunmute @angelpro",
+            "{tr}vcunmute @apro",
             "{tr}vcunmute userid1 userid2",
         ],
     },
