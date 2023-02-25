30ec8b635331c960aca89abb25a023d46e993f47
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-02-25 18:29:27 +0530

    Update stream_helper.py

--- helper/stream_helper.py
+++ helper/stream_helper.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 from requests.exceptions import MissingSchema
 from requests.models import PreparedRequest
-from userbot.utils import runcmd
+from Legendbot.utils import runcmd
 from yt_dlp import YoutubeDL
 
 
